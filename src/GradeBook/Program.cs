495591c347e6438aa3b2751de45f54dd005863e3
Joshua <serkroth@tutanota.com>
2021-03-30 16:04:56 -0500

    Unit 7 update

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,15 +8,28 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Joshua's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            
+            //Loop to ask for grade, q to exit
+            while(true)
+            {
+                Console.WriteLine($"Enter a grade. q to exit");
+                var input = Console.ReadLine();
+
+                if(input == "q")
+                {
+                    break;
+                }
+
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+            }
 
             var stats = book.GetStatistics();
 
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 }
